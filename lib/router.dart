ed46c684ca3a6a154bb4ead4d742ab3c14673af3
noel-srocha <manoelrocha.n93@gmail.com>
2023-02-02 06:48:05 -0300

    Adds BMI page and basic routing

--- lib/router.dart
+++ lib/router.dart
@@ -8,7 +8,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const HomeView();
+        return HomeView();
       },
     ),
   ],
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  double _bmi = 0;
+
+  void _calculateBMI() {
+    setState(() {
+      double weight = double.parse(_weightController.text);
+      double height = double.parse(_heightController.text) / 100;
+      _bmi = weight / (height * height);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _weightController,
+              decoration: InputDecoration(labelText: 'Weight (kg)'),
+            ),
+            TextField(
+              controller: _heightController,
+              decoration: InputDecoration(labelText: 'Height (cm)'),
+            ),
+            ElevatedButton(
+              onPressed: _calculateBMI,
+              child: Text('Calculate'),
+            ),
+            Text('BMI: $_bmi')
+          ],
+        ),
+      ),
+    );
   }
 }
